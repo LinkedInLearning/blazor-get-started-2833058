3db37226204dc1e5756f1db56278a83626a183f2
Rich Goforth <rcgoforth@gmail.com>
2020-06-21 18:33:28 -0400

    04_06 Challenge Completion

--- Beam.Tests/IndexPagesTests.cs
+++ Beam.Tests/IndexPagesTests.cs
@@ -1,10 +1,12 @@
-using System;
 using Xunit;
 using Bunit;
-using Bunit.Mocking.JSInterop;
 using static Bunit.ComponentParameterFactory;
 using Beam.Tests.Auth;
+using Beam.Client.Pages;
 using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Beam.Tests
 {
@@ -27,6 +29,25 @@ namespace Beam.Tests
             Assert.Contains("Select a Frequency, or create a new one", cut.Markup);
             cut.MarkupMatches(@"<h2 diff:ignore></h2>
                                 <p diff:ignore></p>"); 
+        }
+
+
+        [Fact]
+        public void IndexShowsSimpleHeaderWhenUnAuthorized()
+        {
+            Services.AddAuthenticationServices(TestAuthenticationStateProvider.CreateUnauthenticationState(), AuthorizationResult.Failed());
+
+            Services.AddScoped<NavigationManager, MockNavigationManager>();
+
+            var wrapper = RenderComponent<CascadingAuthenticationState>(parameters => parameters.AddChildContent<Index>());
+
+            // Arrange
+            var cut = wrapper.FindComponent<Index>();
+            
+            // Assert that content of the paragraph shows counter at zero
+            Assert.DoesNotContain("Select a Frequency, or create a new one", cut.Markup);
+            
+            cut.Find("a").MarkupMatches(@"<a href=""/login"">Log In</a>");
         }
 
     }
