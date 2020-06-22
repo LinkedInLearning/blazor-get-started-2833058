9b4b47314f6d5d6d3ee71c553db09721f4ce32b1
Rich Goforth <rcgoforth@gmail.com>
2020-06-22 16:46:25 -0400

    06_05e

--- Beam.Client/Program.cs
+++ Beam.Client/Program.cs
@@ -30,6 +30,8 @@ namespace Beam.Client
             builder.Services.AddBlazoredLocalStorage();
 
             builder.Services.AddSingleton<AnimationService>();
+            
+            builder.Services.AddTelerikBlazor();
 
             await builder.Build().RunAsync();
         }
