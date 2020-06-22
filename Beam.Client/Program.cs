dd1efd543936808ab3c6db6c304de8eb420da41a
Rich Goforth <rcgoforth@gmail.com>
2020-06-22 11:41:21 -0400

    06_03 Tasks

--- Beam.Client/Program.cs
+++ Beam.Client/Program.cs
@@ -8,6 +8,9 @@ using Beam.Client.Services;
 using Microsoft.AspNetCore.Components.Authorization;
 using Blazored.LocalStorage;
 using Beam.Animation;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 
 namespace Beam.Client
 {
@@ -17,6 +20,15 @@ namespace Beam.Client
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
+
+            builder.Services
+                  .AddBlazorise(options =>
+                 {
+                     options.ChangeTextOnKeyPress = true;
+                 })
+                  .AddBootstrapProviders()
+                  .AddFontAwesomeIcons();
+
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddTransient<IBeamApiService, BeamApiService>();
             builder.Services.AddSingleton<IDataService, DataService>();
@@ -26,12 +38,18 @@ namespace Beam.Client
             builder.Services.AddScoped<BeamAuthenticationStateProvider>();
             builder.Services.AddScoped<AuthenticationStateProvider>
                 (s => s.GetRequiredService<BeamAuthenticationStateProvider>());
-            
+
             builder.Services.AddBlazoredLocalStorage();
 
             builder.Services.AddSingleton<AnimationService>();
 
-            await builder.Build().RunAsync();
+            var host = builder.Build();
+
+            host.Services
+              .UseBootstrapProviders()
+              .UseFontAwesomeIcons();
+
+            await host.RunAsync();
         }
     }
 }
