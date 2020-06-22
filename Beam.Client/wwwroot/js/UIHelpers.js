d312fce57ce9b1c6d623e2637822f04eb562eb85
Rich Goforth <rcgoforth@gmail.com>
2020-06-22 08:56:02 -0400

    05_05e

--- Beam.Client/wwwroot/js/UIHelpers.js
+++ Beam.Client/wwwroot/js/UIHelpers.js
@@ -1,4 +1,7 @@
 window.UIHelpers = {
     focusElement : function (element) { element.focus(); },
-    focusFirstChild : function (element) {element.firstElementChild.focus();}
+    focusFirstChild : function (element) {element.firstElementChild.focus();},
+    animateElement : function (element, keyFrames, speed) {
+        element.firstElementChild.animate(keyFrames, speed);
+    }
 }
